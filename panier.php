f928b3bd2abc3bd151c752ca241d335c77bc4b4b
Jotawk <Jonathan.tawk@live.fr>
2022-10-14 15:27:09 +0200

    Fix bug supp switch

--- panier.php
+++ panier.php
@@ -62,44 +62,7 @@
                                         $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['dimanche']['dessert'];
                                         break;
                                 }
-                                switch($reservation['jourReservation']) {
-                                    case 'lundi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['lundi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['lundi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['lundi']['dessert'];
-                                        break;
-                                    case 'mardi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['mardi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['mardi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['mardi']['dessert'];
-                                        break;
-                                    case 'mercredi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['mercredi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['mercredi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['mercredi']['dessert'];
-                                        break;
-                                    case 'jeudi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['jeudi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['jeudi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['jeudi']['dessert'];
-                                        break;
-                                    case 'vendredi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['vendredi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['vendredi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['vendredi']['dessert'];
-                                        break;
-                                    case 'samedi':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['samedi']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['samedi']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['samedi']['dessert'];
-                                        break;
-                                    case 'dimanche':
-                                        $_SESSION['Entree'] = $_SESSION['menuSemaine'][0]['dimanche']['entree'];
-                                        $_SESSION['Plat'] = $_SESSION['menuSemaine'][0]['dimanche']['plat'];
-                                        $_SESSION['Dessert'] = $_SESSION['menuSemaine'][0]['dimanche']['dessert'];
-                                        break;
-                            
-                                }
+                                
                                 $ref=$index;
                                 echo "<div class='reservation'>",
                                             "<div class='personnes donnee'<p>".$reservation['jourReservation'] ."</p></div>",
