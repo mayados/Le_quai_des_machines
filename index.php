f6403c476740b792fb536dc27a974236af374388
Jonathan Tawk <Jonathan.Tawk@live.fr>
2022-10-17 15:05:47 +0200

    Ajout option value

--- index.php
+++ index.php
@@ -87,7 +87,6 @@ $_SESSION['menuSemaine'][] = $menuSemaine;
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Restaurant Le Quai des Machines</title>
-    <link rel="shortcut icon" href="img/logo_resto.ico" />
 </head>
 <body>
     <div class="container-page">
