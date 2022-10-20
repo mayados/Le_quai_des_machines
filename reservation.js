f1a459ac38cbd7d2754d42d80ccf38848dfc3634
Jotawk <Jonathan.tawk@live.fr>
2022-10-20 20:28:24 +0200

    Sauvegarde : 20/10/22

--- reservation.js
+++ reservation.js
@@ -1,12 +1,12 @@
-					/* Surbrillance réservation du jour */
-let jours = document.getElementsByClassName('jour-semaine');
+// 					/* Surbrillance réservation du jour */
+// let jours = document.getElementsByClassName('jour-semaine');
 
-/* Transforme les NodeList en array = chaque élément de class */
-let arrJours = Array.from(jours);
+// /* Transforme les NodeList en array = chaque élément de class */
+// let arrJours = Array.from(jours);
 
-arrJours.forEach(function(item){
-    if(item==arrJours[0]){
-		item.setAttribute('selected', 'selected');
-	}
-});
+// arrJours.forEach(function(item){
+//     if(item==arrJours[0]){
+// 		item.setAttribute('selected', 'selected');
+// 	}
+// });
 
