4720d82906a0050ff5ff2bee454a5d02eb5ea9ee
mayados <mariannedos.cours@gmail.com>
2022-10-21 16:47:48 +0200

    html title remove "panier" word

--- panier.php
+++ panier.php
@@ -5,14 +5,14 @@
     showMessage();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/panier.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Panier reservation</title>
+    <title>reservation</title>
 </head>
 <body>
     <div id="container-page">
